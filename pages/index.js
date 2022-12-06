97027d989a8e60f2f24c3f2df0161da9c25aa755
Fernando Dingler <fdingler@amazon.com>
2022-12-06 12:37:40 -0800

    Next.js SSG

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -60,10 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Amplify
         </a>
       </footer>
     </div>
